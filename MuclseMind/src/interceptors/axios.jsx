e3a1d321be7870a7cd58ea1a55fa37ba1784679f
yugandhar0609 <yugandharselvam@gmail.com>
2024-12-14 19:17:33 +0530

    changes

--- MuclseMind/src/interceptors/axios.jsx
+++ MuclseMind/src/interceptors/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "http://localhost:3333",
+  // baseURL: "http://localhost:3333",
   baseURL: "https://muclsemind-ai.onrender.com",
   // baseURL: "http://192.168.1.10:3000",
 });
