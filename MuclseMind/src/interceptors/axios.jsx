11aa07d042c34cf6863a18b1a012112b09157073
yugandhar0609 <yugandharselvam@gmail.com>
2024-12-19 10:54:07 +0530

    backend completed

--- MuclseMind/src/interceptors/axios.jsx
+++ MuclseMind/src/interceptors/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiInstance = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://dentalcrmbackend.vercel.app/",
+  baseURL: "muscle-mind-crm.vercel.app",
 });
 
 apiInstance.interceptors.request.use(
