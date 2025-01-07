8d4c1596b136c54ca4e53ece7f7fcfdff02db9a2
yugandhar0609 <yugandharselvam@gmail.com>
2025-01-07 11:54:54 +0530

    changes

--- MuclseMind/dev-dist/sw.js
+++ MuclseMind/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5lfkbtsi0l8"
+    "revision": "0.9pvcjrl3rn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
